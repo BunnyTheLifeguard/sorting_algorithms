1ec154c82d599e87eb668a37bf060f2abc4a7981
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-06-06 18:29:18 +0200

    Added comments

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -10,20 +10,27 @@ namespace sorting_algorithms
             Console.WriteLine($"Unsorted array: {string.Join(", ", nums)}");
             Console.WriteLine("----------");
 
+            // Variable to know when to stop sorting routine
             bool swapped;
 
             do
             {
+                // Set to false to guarantee at least one pass on array
                 swapped = false;
 
                 for (int i = 0; i < nums.Length - 1; i++)
                 {
+                    // Compare first value with adjacent value
                     if (nums[i] > nums[i + 1])
                     {
                         Console.WriteLine($"Before swap: {string.Join(", ", nums)}");
+                        // Temporarily save the value to the right
                         int temp = nums[i + 1];
+                        // Replace right value with the larger one
                         nums[i + 1] = nums[i];
+                        // Reassign smaller temp value to previous location of the larger one
                         nums[i] = temp;
+                        // Indicate that a swap happened => Continue checking values
                         swapped = true;
                         Console.WriteLine($"After swap: {string.Join(", ", nums)}");
                         Console.WriteLine("----------");
--- InsertionSort.cs
+++ InsertionSort.cs
@@ -10,14 +10,18 @@ namespace sorting_algorithms
             Console.WriteLine($"Unsorted array: {string.Join(", ", nums)}");
             Console.WriteLine("----------");
 
+            // Variable for insertion
             int newValue;
 
             for (int i = 1; i < nums.Length; i++)
             {
                 Console.WriteLine($"Before swap: {string.Join(", ", nums)}");
+                // Start at second element in array (since first one has no preceding value)
                 newValue = nums[i];
+                // Set start of inner loop to same value as i
                 int j = i;
 
+                /* Check value of j to ensure the end of iteration (j = 0) is not reached and if value preceding the current value is larger If true => Swap, decrement j, test next value until (j = 0) */
                 while (j > 0 && nums[j - 1] > newValue)
                 {
                     nums[j] = nums[j - 1];
